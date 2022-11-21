784402b429984da06452d48f8f0dbc8bea4a67d9
BryanLSantos <66854962+BryanLSantos@users.noreply.github.com>
2022-11-20 23:13:47 -0600

    archivo comentado

--- javaScript/conexionArduino.js
+++ javaScript/conexionArduino.js
@@ -1,56 +1,50 @@
+//Mensaje que se enviara a la pagina
 let mensaje = "";
+//libreria de conexion de arduino
 var five = require("johnny-five"),
+//Definicion de la placa y boton en Johnny Five
 board, button;
+//Objeto de placa para Johny five
 board = new five.Board();
+//Funcion que se hara cuando la placa este lista
 board.on("ready", function() {
-// Create a new `button` hardware instance.
-// This example allows the button module to
-// create a completely default instance
+// Crear una nueva instancia de 'button' de hardware.
 buttonA = new five.Button(2);
 buttonB = new five.Button(3);
 buttonC = new five.Button(4);
 buttonD = new five.Button(5);
-// Inject the `button` hardware into
-// the Repl instance's context;
-// allows direct command line access
+// Inyecta el hardware 'boton' dentro
+// el Repl de la instancia del contexto;
+// permite acceso de linea de comando directo
 board.repl.inject({
     buttonA: button,
     buttonB: button,
     buttonC: button,
     buttonD: button
 });
-// Button Event API
-// "down" the button is pressed
-// buttonA.on("down", function() {
-//     console.log("down");
-   
-// });
-// "hold" the button is pressed for specified time.
-//        defaults to 500ms (1/2 second)
-//        set
-// buttonA.on("hold", function() {
-//     console.log("hold");
-//     mensaje = "down";
-// });
-// "up" the button is released
+
+// "up" el boton A en este caso es presionado
 buttonA.on("up", function() {
     console.log("up 1");
     mensaje = "up 1";
     mensaje = "boton1";
     io.emit('respuesta', mensaje );
 });
+// "up" el boton B en este caso es presionado
 buttonB.on("up", function() {
     console.log("up 2");
     mensaje = "up 2";
     mensaje = "boton2";
     io.emit('respuesta', mensaje );
 });
+// "up" el boton C en este caso es presionado
 buttonC.on("up", function() {
     console.log("up 3");
     mensaje = "up 3";
     mensaje = "boton3";
     io.emit('respuesta', mensaje );
 });
+// "up" el boton D en este caso es presionado
 buttonD.on("up", function() {
     console.log("up 4");
     mensaje = "up 4";
@@ -59,8 +53,7 @@ buttonD.on("up", function() {
 });
 });
 
-
-
+//Creacion de objeto para comunicacion de arduino con html y javascript
 const express = require('express');
 const app = express();
 const http = require('http');
@@ -68,31 +61,20 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+//Funcion que automaticamente redigige a una pagina
 app.get('/', (req, res) => {
     res.sendFile('E:/Github/Quiz/Quiz/index.html');
 });
 
+//Funcion que hace cuando se detecta conexion
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
-    // socket.on('respuesta', (botonenvio) => {
-    //   console.log('se recivio del cliente: ' + botonenvio);
-    // });
-    // io.emit('respuesta', { someProperty: '1', otherProperty: 'other value' }); // This will emit the event to all connected socket
-    // // if(botonaux!=boton){
-    //     io.emit('respuesta', { respuesta: boton }); // This will emit the event to all connected sockets
-    // }
-    // socket.on('activador', msg => {
-    // //   io.emit('chat message', msg);
-    //     //var mensaje = parseInt(boton);
-    //     //console.log("boton: " . mensaje);
-    //     mensaje = mensaje.toString();
-    //     io.emit('respuesta', mensaje );
-    // });
 });
 
+//Funcion que escucha el puerto 3200 o 8080
 server.listen(3200, () => {
   console.log('listening on *:3200 or 8080');
 });
